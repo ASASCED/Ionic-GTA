17d83c6cc3cad56c3c9b23b38a9c08aee4fc74a5
Jesus Jimenez Cordero <jesus_heaven117@outlook.com>
2019-05-03 16:44:57 -0500

    Inicio de Firebase en la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
+// NATIVE
+import { Camera } from '@ionic-native/camera/ngx';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+
+// FIREBASE - ANGULAR
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCpJZH3LVkx0amxPOe_ftbbAX1eEJny0Qo',
+  authDomain: 'comida-ad9d7.firebaseapp.com',
+  databaseURL: 'https://comida-ad9d7.firebaseio.com',
+  projectId: 'comida-ad9d7',
+  storageBucket: 'comida-ad9d7.appspot.com',
+  messagingSenderId: '145314079975',
+  appId: '1:145314079975:web:94dd6e5ad8b4d39f'
+};
+// --------------------------
+
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -12,12 +32,22 @@ import { AppRoutingModule } from './app-routing.module';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+  ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    AngularFireDatabase,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Camera,
+    ImagePicker
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
